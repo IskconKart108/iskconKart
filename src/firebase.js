2e8d75534c8b7f755831a278a89f1f9b1f69202e
Rinku Verma <rinkuv37@gmail.com>
2024-03-26 23:43:47 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC0uBOcSmEoDWJLjkhdd0J164cj3JmLsgo",
-  authDomain: "ecommercereact-3c539.firebaseapp.com",
-  projectId: "ecommercereact-3c539",
-  storageBucket: "ecommercereact-3c539.appspot.com",
-  messagingSenderId: "1049927114359",
-  appId: "1:1049927114359:web:26bac549a2a70ec9e8f072"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
